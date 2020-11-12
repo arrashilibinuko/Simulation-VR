3ed3119aa3d60402b346e4a55ad79472068f902c
Mallvin2000 <64111704+Mallvin2000@users.noreply.github.com>
2020-11-12 11:21:48 +0800

    Add play and quit button functionalities as well as logo to menu canvas

---
+++ SimulationVR/Assets/_MyProject/Scripts/AppPlay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//-----------------------------------------------------------------------------------------------------
+//Author: Mallvin Rajamohan
+//Description of code: Scene is launched when method is run. This script should be attatched to the play button
+//-----------------------------------------------------------------------------------------------------
+public class AppPlay : MonoBehaviour
+{
+   public void PlayScene(string scenename)//used to launch scene when method is called
+   {
+       Application.LoadLevel (scenename);//name of scene to be launched is put into scenename
+   }
+}
---
+++ SimulationVR/Assets/_MyProject/Scripts/AppQuit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+//-----------------------------------------------------------------------------------------------------
+//Author: Mallvin Rajamohan
+//Description of code: To exit the application when the method is called. This script is to be attatched to the quit button
+//-----------------------------------------------------------------------------------------------------
+
+public class AppQuit : MonoBehaviour {
+
+	public void Exit()//applicatiom is closed when method is called
+	{
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
+	}
+}
