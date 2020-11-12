abbab8da755a7c9858845f26e7755dbed3799f2c
Mallvin2000 <64111704+Mallvin2000@users.noreply.github.com>
2020-11-12 11:44:21 +0800

    Added splash screen script and build settings updated
    Need to change splash screen canvas render mode back to world space if using vr. For editor testing, i set it to screen space - overlay

---
+++ SimulationVR/Assets/_MyProject/Scripts/SplashScreenSceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//-----------------------------------------------------------------------------------------------------
+//Author: Mallvin Rajamohan
+//Description of code: To Make the splash screen appear with a delay of 5 seconds when the application is launched. This should be set to index 0 during build
+//-----------------------------------------------------------------------------------------------------
+
+public class SplashScreenSceneLoader : MonoBehaviour
+{
+    #region PUBLIC_MEMBERS
+    public float loadingDelay = 5.0F;//delay before the splash screen dissapears
+    #endregion //PUBLIC_MEMBERS
+
+
+    #region MONOBEHAVIOUR_METHODS
+    void Start()
+    {
+        StartCoroutine(LoadNextSceneAfter(loadingDelay));
+    }
+    #endregion //MONOBEHAVIOUR_METHODS
+
+
+    #region PRIVATE_METHODS
+    private IEnumerator LoadNextSceneAfter(float seconds)//Next scene in index 1 position in build is launched after the set delay
+    {
+        yield return new WaitForSeconds(seconds);
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    #endregion //PRIVATE_METHODS
+}
