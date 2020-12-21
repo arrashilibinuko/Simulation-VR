2a44971db1b8e1c16a4bcaf6e89d4a45b129a809
arrashilibinuko <58247631+arrashilibinuko@users.noreply.github.com>
2020-12-21 13:33:14 +0800

    get device name

--- SimulationVR/Assets/_MyProject/Scripts/TaskList.cs
+++ SimulationVR/Assets/_MyProject/Scripts/TaskList.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class TaskList : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject TaskBoard;
+    private InputDevice targetDevice;
+
+    private void Start()
     {
-        
+        List <InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        foreach(var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
+
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
