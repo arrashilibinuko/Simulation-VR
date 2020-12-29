a947da39fdba7f01e7c8730e5fe618926cdf7bfe
Mallvin2000 <64111704+Mallvin2000@users.noreply.github.com>
2020-12-29 17:56:51 +0800

    setting up audio mixer and ambient audio and settings panel

---
+++ SimulationVR/Assets/_MyProject/Scripts/mixLevels.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+//-----------------------------------------------------------------------------------------------------
+//Author: Mallvin Rajamohan
+//Description of code: To adjust audio mixer levels through menu with exposed params
+//-----------------------------------------------------------------------------------------------------
+
+public class mixLevels : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+
+    public void SetMasterLvl(float masterlvl) {
+        masterMixer.SetFloat("MasterVol", masterlvl);//set master vol to value of slider
+    }
+
+    public void SetBGMLvl(float bgmlvl) {
+        masterMixer.SetFloat("BGMVol", bgmlvl);//set background music vol to value of slider
+    }
+}
